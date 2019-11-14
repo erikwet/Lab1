42bb5a0608a55c7e5f50bc02a62073ce9fb691d7
oscarvid <o.j.arvidson@hotmail.com>
2019-11-14 14:26:40 +0100

    schack

--- Test/MovableTest.java
+++ Test/MovableTest.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import java.util.List;
 
 public class MovableTest {
 
--- src/Car.java
+++ src/Car.java
@@ -11,8 +11,8 @@ public abstract class Car implements Movable {
     private Color color; // Color of the car
     private final String modelName; // The car model name
     private Direction currentDirection = Direction.NORTH; // Direction the car is facing
-    double x; // x position of car
-    double y; // y position of car
+    protected double x; // x position of car
+    protected double y; // y position of car
 
     /**
      * Constructor for Car class
